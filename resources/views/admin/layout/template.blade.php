5f9701f3300b82e040a463f79cca0ee2e7256a2b
Josue Choque Gomez <110512792+JosueChoqueG@users.noreply.github.com>
2023-11-08 10:58:32 -0500

    Update template.blade.php
    para http y https

--- resources/views/admin/layout/template.blade.php
+++ resources/views/admin/layout/template.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
     <meta name="author" content="Łukasz Holeczek">
