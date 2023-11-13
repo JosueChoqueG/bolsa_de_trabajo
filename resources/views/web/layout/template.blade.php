0d5e1336734037de192d1531cdd6303d4380c737
josue choque gomez <182197@unamba.edu.pe>
2023-11-13 11:09:56 -0500

    secure asset head resources

--- resources/views/web/layout/template.blade.php
+++ resources/views/web/layout/template.blade.php
@@ -6,30 +6,30 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>BTUNAMBA</title>
-    <link rel="icon" href="{{asset('img/principal/logo.png')}}">
+    <link rel="icon" href="{{secure_asset('img/principal/logo.png')}}">
     <!-- Bootstrap CSS -->
      
-    <link href="{{asset('assets_admin/css/coreui-icons.min.css')}}" rel="stylesheet">
-    <link href="{{asset('assets_admin/css/flag-icon.min.css')}}" rel="stylesheet">
-    <link href="{{asset('assets_admin/css/simple-line-icons.css')}}" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href=" {{ asset('assets_admin/plugins/datatables.net-bs4/css/dataTables.bootstrap4.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets_web/css/custom.css')}}">
-    <link rel="stylesheet" href="{{asset('assets_web/develop_css/principal.css')}}">
-    <link rel="stylesheet" href="{{asset('assets_web/develop_css/components.css')}}">
-    <link rel="stylesheet" href="{{asset('assets_web/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets_web/css/style.css?1')}}">
-    <link rel="stylesheet" href="{{asset('assets_web/css/animate.css')}}">
-    <link rel="stylesheet" href="{{asset('assets_web/css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets_web/css/themify-icons.css')}}">
-    <link rel="stylesheet" href="{{asset('assets_web/css/font-awesome.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets_web/css/LineIcons.css')}}">
-    <link rel="stylesheet" href="{{asset('assets_web/css/flaticon.css')}}">
+    <link href="{{secure_asset('assets_admin/css/coreui-icons.min.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('assets_admin/css/flag-icon.min.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('assets_admin/css/simple-line-icons.css')}}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href=" {{ secure_asset('assets_admin/plugins/datatables.net-bs4/css/dataTables.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets_web/css/custom.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets_web/develop_css/principal.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets_web/develop_css/components.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets_web/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets_web/css/style.css?1')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets_web/css/animate.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets_web/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets_web/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets_web/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets_web/css/LineIcons.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets_web/css/flaticon.css')}}">
 
-    <link href="{{ asset('assets_admin/css/toastr2.min.css')}}"rel='stylesheet'type='text/css'> 
-    <link rel="stylesheet" href="{{asset('assets_web/css/magnific-popup.css')}}">
-    <link rel="stylesheet" href="{{asset('assets_admin/plugins/select2/dist/css/select2.min.css')}}" type='text/css' >
+    <link href="{{ secure_asset('assets_admin/css/toastr2.min.css')}}"rel='stylesheet'type='text/css'> 
+    <link rel="stylesheet" href="{{secure_asset('assets_web/css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets_admin/plugins/select2/dist/css/select2.min.css')}}" type='text/css' >
    
-    <link rel="stylesheet" href="{{ asset('assets_admin/plugins/sweetalert/css/sweetalert.css') }}"/>
+    <link rel="stylesheet" href="{{ secure_asset('assets_admin/plugins/sweetalert/css/sweetalert.css') }}"/>
    
     
     @section('css')
@@ -116,30 +116,30 @@
     </form>
     <!-- jquery plugins here-->
     <!-- jquery -->
-    <script src="{{asset('assets_admin/js/jquery.min.js')}}"></script>
+    <script src="{{secure_asset('assets_admin/js/jquery.min.js')}}"></script>
     <!-- popper js -->
-    <script src="{{asset('assets_web/js/popper.min.js')}}"></script>
+    <script src="{{secure_asset('assets_web/js/popper.min.js')}}"></script>
     <!-- bootstrap js -->
-    <script src="{{asset('assets_web/js/bootstrap.min.js')}}"></script>
+    <script src="{{secure_asset('assets_web/js/bootstrap.min.js')}}"></script>
     <!-- counterup js -->
-    <script src="{{asset('assets_web/js/jquery.counterup.min.js')}}"></script>
+    <script src="{{secure_asset('assets_web/js/jquery.counterup.min.js')}}"></script>
     <!-- waypoints js -->
-    <script src="{{asset('assets_web/js/waypoints.min.js')}}"></script>
+    <script src="{{secure_asset('assets_web/js/waypoints.min.js')}}"></script>
     <!-- easing js -->
-    <script src="{{asset('assets_web/js/jquery.magnific-popup.js')}}"></script>
+    <script src="{{secure_asset('assets_web/js/jquery.magnific-popup.js')}}"></script>
     <!-- particles js -->
-    <script src="{{asset('assets_web/js/owl.carousel.min.js')}}"></script>
+    <script src="{{secure_asset('assets_web/js/owl.carousel.min.js')}}"></script>
     <!-- custom js -->
-    <script src="{{asset('assets_web/js/custom.js')}}"></script>
+    <script src="{{secure_asset('assets_web/js/custom.js')}}"></script>
     {{-- Toster --}}
-    <script src="{{asset('assets_admin/js/toastr.min.js')}}"></script>
+    <script src="{{secure_asset('assets_admin/js/toastr.min.js')}}"></script>
     
-    <script src="{{asset('assets_web/develop_js/general_jscript.js')}}"></script>
-    <script src="{{asset('assets_web/develop_js/general_jquery.js')}}"></script>
+    <script src="{{secure_asset('assets_web/develop_js/general_jscript.js')}}"></script>
+    <script src="{{secure_asset('assets_web/develop_js/general_jquery.js')}}"></script>
     
-    <script src="{{asset('assets_web/develop_js/home/job_offer_jquery.js')}}"></script>
-    <script src="{{asset('assets_web/develop_js/home/job_offer_jscript.js')}}"></script>
-    <script type="text/javascript" src=" {{ asset('assets_admin/plugins/sweetalert/js/sweetalert.min.js') }}"></script>
+    <script src="{{secure_asset('assets_web/develop_js/home/job_offer_jquery.js')}}"></script>
+    <script src="{{secure_asset('assets_web/develop_js/home/job_offer_jscript.js')}}"></script>
+    <script type="text/javascript" src=" {{ secure_asset('assets_admin/plugins/sweetalert/js/sweetalert.min.js') }}"></script>
     <script>
         var csrf_token   = "{{csrf_token()}}";
         var base_url     = "{{url('')}}";
